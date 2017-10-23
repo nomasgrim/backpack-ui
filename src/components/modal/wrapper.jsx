ea9ffe3b346662766275b87f111c5bf192730aa1
Tom Cunningham <thomasthesecond@users.noreply.github.com>
2017-10-23 12:18:10 -0400

    fix: Fix bug in iOS; screen appears blank when modal opens (#538)
    * Create static scrollTo method that checks if window is defined
    
    * Create scrollPosition state
    
    * Scroll window to top, set scrollPosition state when modal state is open
    
    This is done to fix a bug seen in iOS Chrome and Safari where the
    on-screen keyboard ruins the scroll position of the modal when
    opened and appears as if the screen is blank.
    
    When the modal is open, noScroll is turned on, which calculates the
    current scroll position and sets this value to html.style.top. This
    value is transformed and set to the scrollPosition state. Lastly,
    the scrollTo method is used to scroll the window to top and prevent
    the keyboard from causing the scroll bar to miscalculate the window
    height.
    
    * Scroll to scrollPosition state when the modal open state is false
    
    When the modal is closed, the window is scrolled back to where the
    user was before the modal was opened. This creates a better experience
    and prevents an annoying scroll jump when the modal is closed. This is
    just the value that is calculated by noScroll.on() and stored in the
    state when the modal opens.

--- src/components/modal/wrapper.jsx
+++ src/components/modal/wrapper.jsx
@@ -3,11 +3,18 @@ import PropTypes from "prop-types";
 import noScroll from "no-scroll";
 
 class ModalWrapper extends React.Component {
+  static scrollTo = (x = 0, y = 0) => {
+    if (typeof window !== "undefined") {
+      window.scrollTo(x, y);
+    }
+  }
+
   constructor(props) {
     super(props);
 
     this.state = {
       open: false,
+      scrollPosition: 0,
     };
 
     this.toggleOpen = this.toggleOpen.bind(this);
@@ -28,8 +35,13 @@ class ModalWrapper extends React.Component {
     }, () => {
       if (this.state.open) {
         noScroll.on();
+        ModalWrapper.scrollTo(0, 0);
+        this.setState({
+          scrollPosition: parseInt(document.documentElement.style.top.replace("px", ""), 10) * -1,
+        });
       } else {
         noScroll.off();
+        ModalWrapper.scrollTo(0, this.state.scrollPosition);
       }
     });
   }
