dc3ba64ef1ab1ee299f00bff222b53885eb66a13
laserchicken <mateusz.kmiecinski@lonelyplanet.com>
2017-11-14 15:24:43 +0000

    fix: fix avatar uploader (use exif orientation)

--- src/components/avatarUpload/index.jsx
+++ src/components/avatarUpload/index.jsx
@@ -61,25 +61,79 @@ const scopedStyles = {
 
 class AvatarUpload extends Component {
 
-  static dataURItoBlob(dataURI) {
-    // convert base64/URLEncoded data component to raw binary data held in a string
+  static strToTypedArray(str) {
     let byteString;
-    if (dataURI.split(",")[0].indexOf("base64") >= 0) {
-      byteString = atob(dataURI.split(",")[1]);
+    if (str.split(",")[0].indexOf("base64") >= 0) {
+      byteString = atob(str.split(",")[1]);
     } else {
-      byteString = unescape(dataURI.split(",")[1]);
+      byteString = unescape(str.split(",")[1]);
     }
-
-    // separate out the mime component
-    const mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
 
     // write the bytes of the string to a typed array
     const ia = new Uint8Array(byteString.length);
     for (let i = 0; i < byteString.length; i += 1) {
       ia[i] = byteString.charCodeAt(i);
     }
+    return ia;
+  }
+
+  static dataURItoBlob(dataURI) {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    const ia = AvatarUpload.strToTypedArray(dataURI);
+
+    // separate out the mime component
+    const mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
 
     return new Blob([ia], { type: mimeString });
+  }
+
+  static getOrientation(src) {
+    const view = new DataView(AvatarUpload.strToTypedArray(src).buffer);
+    if (view.getUint16(0, false) !== 0xFFD8) return -2;
+    const length = view.byteLength;
+    let offset = 2;
+    while (offset < length) {
+      const marker = view.getUint16(offset, false);
+      offset += 2;
+      if (marker === 0xFFE1) {
+        offset += 2;
+        if (view.getUint32(offset, false) !== 0x45786966) return -1;
+        const little = view.getUint16(offset += 6, false) === 0x4949;
+        offset += view.getUint32(offset + 4, little);
+        const tags = view.getUint16(offset, little);
+        offset += 2;
+        for (let i = 0; i < tags; i += 1) {
+          if (view.getUint16(offset + (i * 12), little) === 0x0112) {
+            return view.getUint16(offset + (i * 12) + 8, little);
+          }
+        }
+      } else if ((marker & 0xFF00) !== 0xFF00) break; // eslint-disable-line no-bitwise
+      else offset += view.getUint16(offset, false);
+    }
+    return -1;
+  }
+
+  static resetOrientation(ctx, orientation) {
+    const width = ctx.canvas.width;
+    const height = ctx.canvas.height;
+
+    // set proper canvas dimensions before transform
+    if (orientation > 4 && orientation < 9) {
+      ctx.canvas.width = height;
+      ctx.canvas.height = width;
+    }
+
+    // transform context before drawing image
+    switch (orientation) {
+      case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
+      case 3: ctx.transform(-1, 0, 0, -1, width, height); break;
+      case 4: ctx.transform(1, 0, 0, -1, 0, height); break;
+      case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+      case 6: ctx.transform(0, 1, -1, 0, height, 0); break;
+      case 7: ctx.transform(0, -1, -1, 0, height, width); break;
+      case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
+      default: break;
+    }
   }
 
   constructor(props) {
@@ -113,12 +167,15 @@ class AvatarUpload extends Component {
         const ctx = await converterCanvas.getContext("2d");
         const converterImage = new Image();
         converterImage.src = e.target.result;
+        const orientation = await AvatarUpload.getOrientation(e.target.result);
 
         converterImage.onload = async () => {
           const targetWidth = 160;
           ctx.canvas.width = targetWidth;
           ctx.canvas.height = converterImage.height * (targetWidth / converterImage.width);
-          await ctx.drawImage(converterImage, 0, 0, targetWidth, ctx.canvas.height);
+          const originalCtxHeight = ctx.canvas.height;
+          await AvatarUpload.resetOrientation(ctx, orientation);
+          await ctx.drawImage(converterImage, 0, 0, targetWidth, originalCtxHeight);
           const convertedUrl = await converterCanvas.toDataURL("image/jpeg");
           const blobImage = await AvatarUpload.dataURItoBlob(convertedUrl);
 
