6df3b0f239523a6e03cce702efa005571a98bd8f
Bartek <barfek@gmail.com>
2017-11-22 16:32:59 +0100

    Fix website link display

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -3,8 +3,7 @@
 /* eslint max-len: 0 */
 
 export const emailValidation = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-export const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,16}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
+export const urlRegex = /(https?:\/\/)((?:www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,16}\b)([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
 
 export default {
   emailValidation,
