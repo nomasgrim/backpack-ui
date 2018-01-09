56a3213c6585371a2eb48f155bd5fc49e7f992c3
Tom Cunningham <thomasthesecond@users.noreply.github.com>
2018-01-09 16:12:01 -0600

    feat: Update IconButton component (#592)

--- src/components/iconButton/index.jsx
+++ src/components/iconButton/index.jsx
@@ -1,46 +1,64 @@
 import React from "react";
 import PropTypes from "prop-types";
-import radium from "radium";
-import upperFirst from "lodash/upperFirst";
+import radium, { Style } from "radium";
+import { upperFirst, uniqueId } from "lodash";
 import cn from "classnames";
-
 import colors from "../../styles/colors";
 import timing from "../../styles/timing";
 import { lineHeightReset } from "../../styles/typography";
+import zIndex from "../../styles/zIndex";
 import { darken, rgba } from "../../utils/color";
 import iconFromString from "../../utils/icon";
 import { outline } from "../../utils/mixins";
 import propTypes from "../../utils/propTypes";
 
-const _ = { upperFirst };
+const _ = { upperFirst, uniqueId };
 
 const styles = {
-  alignItems: "center",
-  backgroundColor: darken(colors.bgPrimary, 6),
-  backfaceVisibility: "hidden",
-  border: 0,
-  borderRadius: "100%",
-  color: colors.textPrimary,
-  cursor: "pointer",
-  display: "inline-flex",
-  justifyContent: "center",
-  lineHeight: lineHeightReset,
-  padding: 0,
-  position: "relative",
-  textAlign: "center",
-  textDecoration: "none",
-  transition: `background-color ${timing.default}`,
-  verticalAlign: "middle",
-  whiteSpace: "nowrap",
-  WebkitTapHighlightColor: rgba(colors.bgOverlay, 0.04),
+  container: {
+    alignItems: "center",
+    backgroundColor: "transparent",
+    backfaceVisibility: "hidden",
+    border: 0,
+    color: colors.textPrimary,
+    cursor: "pointer",
+    display: "inline-flex",
+    justifyContent: "center",
+    lineHeight: lineHeightReset,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    position: "relative",
+    textAlign: "center",
+    textDecoration: "none",
+    transition: `color ${timing.fast}`,
+    verticalAlign: "middle",
+    whiteSpace: "nowrap",
+    WebkitTapHighlightColor: rgba(colors.bgOverlay, 0.04),
 
-  ":focus": outline(4),
-};
+    ":focus": outline(4),
+  },
 
-const iconStyle = {
-  display: "block",
-  marginLeft: "auto",
-  marginRight: "auto",
+  background: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    width: "100%",
+    height: "100%",
+    border: 0,
+    borderRadius: "100%",
+    backgroundColor: darken(colors.bgPrimary, 6),
+  },
+
+  icon: {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    zIndex: zIndex.default,
+  },
 };
 
 const fontSize = {
@@ -49,59 +67,111 @@ const fontSize = {
   56: 24,
 };
 
-function IconButton({
-  iconName,
-  label,
-  id,
-  className,
-  href,
-  onClick,
-  size,
-  owns,
-  backgroundColor,
-  color,
-  border,
-  shadow,
-  style,
-}) {
-  const Element = href ? "a" : "button";
-  const role = Element === "a" ? "button" : null;
-  const dimensions = {
-    fontSize: fontSize[size],
-    height: `${size}px`,
-    width: `${size}px`,
-  };
+class IconButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.id = _.uniqueId("IconButton");
+  }
 
-  return (
-    <Element
-      id={id}
-      className={cn("IconButton", className)}
-      style={[
-        styles,
-        dimensions,
-        backgroundColor && { backgroundColor },
-        color && { color },
-        border && { border: "1px solid currentColor" },
-        shadow && {
-          boxShadow: `${rgba(colors.bgOverlay, 0.2)} 0 ${4 / fontSize[size]}em ${16 / fontSize[size]}em`,
-          transition: `box-shadow ${timing.fast} ease-in-out`,
+  render() {
+    const {
+      iconName,
+      label,
+      id,
+      className,
+      href,
+      onClick,
+      size,
+      owns,
+      backgroundColor,
+      color,
+      hoverBackgroundColor,
+      hoverBackgroundScale,
+      hoverColor,
+      border,
+      shadow,
+      transitionDuration,
+      style,
+    } = this.props;
 
-          ":active": {
-            boxShadow: `${rgba(colors.bgOverlay, 0.2)} 0 ${(4 / fontSize[size]) / 3}em ${(16 / fontSize[size]) / 2}em`,
-          },
-        },
-        style,
-      ]}
-      href={href}
-      onClick={onClick}
-      role={role}
-      title={label}
-      aria-label={label}
-      aria-owns={owns}
-    >
-      {iconFromString(_.upperFirst(iconName), { label, style: iconStyle })}
-    </Element>
-  );
+    const Element = href ? "a" : "button";
+    const role = Element === "a" ? "button" : null;
+    const elementId = id || this.id;
+    const dimensions = {
+      fontSize: fontSize[size],
+      height: `${size}px`,
+      width: `${size}px`,
+    };
+
+    return (
+      <Element
+        id={elementId}
+        className={cn("IconButton", className)}
+        style={[
+          styles.container,
+          { transition: `color ${transitionDuration}` },
+          dimensions,
+          color && { color },
+          hoverColor && { ":hover": { color: hoverColor } },
+          style,
+        ]}
+        href={href}
+        onClick={onClick}
+        role={role}
+        title={label}
+        aria-label={label}
+        aria-owns={owns}
+      >
+
+        {hoverBackgroundColor &&
+          <Style
+            scopeSelector={`#${elementId}:hover`}
+            rules={{
+              [`.${cn("IconButton", className)}-background`]: {
+                backgroundColor: `${hoverBackgroundColor} !important`,
+              },
+            }}
+          />
+        }
+
+        {hoverBackgroundScale &&
+          <Style
+            scopeSelector={`#${elementId}:hover`}
+            rules={{
+              [`.${cn("IconButton", className)}-background`]: {
+                transform: `scale(${hoverBackgroundScale})`,
+              },
+            }}
+          />
+        }
+
+        {shadow &&
+          <Style
+            scopeSelector={`#${elementId}:active`}
+            rules={{
+              [`.${cn("IconButton", className)}-background`]: {
+                boxShadow: `${rgba(colors.bgOverlay, 0.2)} 0 ${(4 / fontSize[size]) / 3}em ${(16 / fontSize[size]) / 2}em !important`,
+              },
+            }}
+          />
+        }
+
+        <div
+          key={1}
+          className={cn("IconButton-background", className)}
+          style={[
+            styles.background,
+            backgroundColor && { backgroundColor },
+            { transition: `background-color ${transitionDuration}, transform ${transitionDuration}, box-shadow ${transitionDuration} ease-in-out` },
+            border && { border: "1px solid currentColor" },
+            shadow && { boxShadow: `${rgba(colors.bgOverlay, 0.2)} 0 ${4 / fontSize[size]}em ${16 / fontSize[size]}em` },
+          ]}
+        />
+
+        {iconFromString(_.upperFirst(iconName), { label, style: styles.icon })}
+      </Element>
+    );
+  }
 }
 
 IconButton.propTypes = {
@@ -110,7 +180,11 @@ IconButton.propTypes = {
     "BookmarkActive",
     "BookmarkAlt",
     "BookmarkAltActive",
+    "ChevronLeft",
+    "ChevronRight",
+    "ClockOutline",
     "Ellipsis",
+    "Play",
     "Share",
   ]).isRequired,
   label: PropTypes.string.isRequired,
@@ -122,8 +196,12 @@ IconButton.propTypes = {
   owns: PropTypes.string,
   backgroundColor: PropTypes.string,
   color: PropTypes.string,
+  hoverBackgroundColor: PropTypes.string,
+  hoverBackgroundScale: PropTypes.number,
+  hoverColor: PropTypes.string,
   border: PropTypes.bool,
   shadow: PropTypes.bool,
+  transitionDuration: PropTypes.string,
   style: propTypes.style,
 };
 
@@ -134,10 +212,12 @@ IconButton.defaultProps = {
   onClick: null,
   size: 32,
   owns: null,
-  backgroundColor: "",
   color: "",
+  hoverColor: "",
+  hoverBackgroundScale: null,
   border: false,
   shadow: false,
+  transitionDuration: timing.default,
   style: null,
 };
 
