06660bc745c16c26ccce416af943cb7f79c86cd8
Tom Cunningham <thomasthesecond@users.noreply.github.com>
2018-01-10 10:54:52 -0600

    feat: Update SpotlightZone component (#597)
    * Remove category prop
    * Remove href prop
    * Remove imageUrl prop
    * Remove background overlay
    * Add videoEmbed prop
    * Add style prop
    * Add VideoEmbed, VideoPopout components
    * Update styles

--- src/components/spotlightZone/index.jsx
+++ src/components/spotlightZone/index.jsx
@@ -1,261 +1,192 @@
 import React from "react";
 import PropTypes from "prop-types";
 import radium from "radium";
-
+import Heading from "../heading";
+import Container from "../container";
+import VideoPopout from "../video/videoPopout";
+import VideoEmbed from "../video/videoEmbed";
+import mq from "../../styles/mq";
+import {
+  fontSizeHeading3,
+  lineHeightHeading3,
+  fontSizeHeading4,
+  lineHeightHeading4,
+  fontSizeHeading5,
+  lineHeightHeading5,
+  fontSizeHeading6,
+  lineHeightHeading6,
+  fontSizeBodyArticleSmall,
+  lineHeightBodyArticleSmall,
+  fontSizeBodySmall,
+  lineHeightBodySmall,
+  fontWeightLight,
+  fontWeightMedium,
+} from "../../styles/typography";
 import colors from "../../styles/colors";
 import dimensions from "../../styles/dimensions";
-import mq from "../../styles/mq";
-import timing from "../../styles/timing";
-import { fontWeightMedium } from "../../styles/typography";
 import zIndex from "../../styles/zIndex";
-import { rgba } from "../../utils/color";
 import font from "../../utils/font";
-import { gutter, percentage } from "../../utils/grid";
-import Link from "../link";
-import Heading from "../heading";
-import { Play } from "../icon";
-import Container from "../container";
+import { percentage, gutter } from "../../utils/grid";
+import propTypes from "../../utils/propTypes";
 
 const styles = {
   container: {
+    backgroundColor: "#1f1f1f",
+    color: colors.bgPrimary,
+    display: "flex",
     fontFamily: font("benton"),
     position: "relative",
-    backgroundColor: colors.bgOverlay,
-    color: colors.bgPrimary,
-
-    [`@media (min-width: ${mq.min[960]})`]: {
-      display: "flex",
-      maxHeight: "100vh",
-      minHeight: "665px",
-    },
   },
 
   background: {
-    position: "absolute",
-    filter: "grayscale(100%)",
     backgroundSize: "cover",
     backgroundPosition: "center",
-    width: "100%",
+    filter: "grayscale(100%)",
     height: "100%",
+    left: 0,
     opacity: 0.4,
-    zIndex: zIndex.default,
-    top: 0,
-    left: 0,
-    right: 0,
-  },
-
-  backgroundOverlay: {
-    backgroundColor: rgba(colors.bgOverlay, 0.38),
-    bottom: 0,
-    height: "100%",
-    left: 0,
     position: "absolute",
+    right: 0,
     top: 0,
-    width: percentage("598px", "1510px"),
+    width: "100%",
     zIndex: zIndex.default,
-
-    [`@media (max-width: ${mq.max[960]})`]: {
-      display: "none",
-    },
   },
 
   content: {
+    display: "flex",
+    paddingBottom: gutter("static"),
+    paddingTop: gutter("static"),
+    width: "100%",
     zIndex: (zIndex.default + 1),
 
-    [`@media (min-width: ${mq.min[960]})`]: {
-      display: "flex",
-      alignItems: "center",
+    [`@media (max-width: ${mq.max["960"]})`]: {
+      flexDirection: "column",
+    },
+
+    [`@media (min-width: ${mq.min["960"]})`]: {
+      paddingBottom: "60px",
+      paddingTop: "56px",
     },
   },
 
   leftContent: {
-    [`@media (max-width: ${mq.max[480]})`]: {
-      marginLeft: `-${gutter("static", 1, 0.5)}`,
-      marginRight: `-${gutter("static", 1, 0.5)}`,
-      paddingLeft: gutter("static", 1, 0.5),
-      paddingRight: gutter("static", 1, 0.5),
-    },
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    position: "relative",
 
-    [`@media (min-width: ${mq.min[480]}) and (max-width: ${mq.max[960]})`]: {
-      marginLeft: `-${gutter("static")}`,
-      marginRight: `-${gutter("static")}`,
-      paddingLeft: gutter("static"),
-      paddingRight: gutter("static"),
-    },
-
-    [`@media (max-width: ${mq.max[600]})`]: {
-      paddingBottom: "32px",
-      paddingTop: "34px",
-    },
-
-    [`@media (min-width: ${mq.min[600]}) and (max-width: ${mq.max[960]})`]: {
-      paddingBottom: "82px",
-      paddingTop: "84px",
-    },
-
-    [`@media (max-width: ${mq.max[960]})`]: {
-      backgroundColor: rgba(colors.bgOverlay, 0.38),
-    },
-
-    [`@media (min-width: ${mq.min[960]})`]: {
-      marginRight: "84px",
-      marginTop: "25px",
-      paddingRight: "50px",
+    [`@media (min-width: ${mq.min["960"]})`]: {
+      paddingRight: "80px",
     },
   },
 
   rightContent: {
-    [`@media (max-width: ${mq.max[600]})`]: {
-      paddingBottom: "38px",
-      paddingTop: "32px",
+    display: "flex",
+    flex: "0 0 auto",
+    flexDirection: "column",
+    justifyContent: "center",
+
+    [`@media (max-width: ${mq.max["480"]})`]: {
+      paddingTop: gutter("static", 1, 0.5),
     },
 
-    [`@media (min-width: ${mq.min[600]}) and (max-width: ${mq.max[960]})`]: {
-      paddingBottom: "88px",
-      paddingTop: "82px",
+    [`@media (min-width: ${mq.min["480"]}) and (max-width: ${mq.max["960"]})`]: {
+      paddingTop: gutter("static"),
     },
 
-    [`@media (min-width: ${mq.min[960]})`]: {
-      flex: "0 0 auto",
+    [`@media (min-width: ${mq.min["960"]})`]: {
       width: percentage("718px", `${dimensions.maxWidth}px`),
     },
   },
 
-  zone: {
-    fontSize: "16px",
-    top: "32px",
-    position: "absolute",
-    fontWeight: fontWeightMedium,
-    lineHeight: 1,
+  leftContentTop: {
+    paddingBottom: gutter("static"),
+  },
 
-    [`@media (min-width: ${mq.min[600]})`]: {
-      fontSize: "24px",
-      top: "64px",
+  leftContentMiddle: {
+    display: "flex",
+    flexDirection: "column",
+    flexGrow: 1,
+    justifyContent: "center",
+
+    [`@media (min-width: ${mq.min["720"]}) and (max-width: ${mq.max["960"]})`]: {
+      flexDirection: "row",
+      justifyContent: "flex-start",
+      paddingBottom: 0,
     },
 
-    [`@media (min-width: ${mq.min[960]})`]: {
-      top: "80px",
+    [`@media (min-width: ${mq.min["960"]})`]: {
+      paddingBottom: "60px",
+    },
+  },
+
+  adSlot: {
+    float: "right",
+
+    [`@media (min-width: ${mq.min["960"]})`]: {
+      float: "none",
+      paddingBottom: "8px",
+      paddingTop: "32px",
+    },
+  },
+
+  zone: {
+    display: "inline-block",
+    fontSize: "18px",
+    fontWeight: fontWeightMedium,
+    left: 0,
+    lineHeight: lineHeightHeading6,
+    position: "relative",
+    top: 0,
+
+    [`@media (min-width: ${mq.min["720"]})`]: {
+      fontSize: `${fontSizeHeading6}px`,
     },
   },
 
   title: {
-    color: colors.bgPrimary,
-    fontSize: "20px",
-    lineHeight: (24 / 20),
+    color: colors.textOverlay,
+    fontSize: `${fontSizeHeading3}px`,
+    lineHeight: lineHeightHeading3,
 
-    [`@media (max-width: ${mq.max[600]})`]: {
-      marginTop: "8px",
+    [`@media (min-width: ${mq.min["720"]}) and (max-width: ${mq.max["960"]})`]: {
+      fontSize: `${fontSizeHeading4}px`,
+      lineHeight: lineHeightHeading4,
+      paddingRight: "20px",
+      width: "50%",
     },
 
-    [`@media (min-width: ${mq.min[600]})`]: {
-      fontSize: "40px",
-      lineHeight: (48 / 40),
-      marginTop: "10px",
-    },
-  },
-
-  category: {
-    color: colors.bgPrimary,
-    fontSize: "11px",
-    lineHeight: 1,
-
-    [`@media (max-width: ${mq.max[600]})`]: {
-      marginTop: "76px",
-    },
-
-    [`@media (min-width: ${mq.min[600]}) and (max-width: ${mq.max[960]})`]: {
-      marginTop: "147px",
-    },
-
-    [`@media (min-width: ${mq.min[600]})`]: {
-      fontSize: "16px",
+    [`@media (max-width: ${mq.max["720"]})`]: {
+      fontSize: `${fontSizeHeading5}px`,
+      lineHeight: lineHeightHeading5,
     },
   },
 
   paragraph: {
-    fontSize: "12px",
-    fontWeight: 300,
-    lineHeight: (20 / 12),
-    marginBottom: 0,
-    marginTop: 0,
+    fontSize: `${fontSizeBodyArticleSmall}px`,
+    fontWeight: fontWeightLight,
+    lineHeight: lineHeightBodyArticleSmall,
+    marginTop: "24px",
 
-    [`@media (max-width: ${mq.max[600]})`]: {
-      marginTop: "18px",
+    [`@media (min-width: ${mq.min["720"]}) and (max-width: ${mq.max["960"]})`]: {
+      marginTop: 0,
+      paddingLeft: "20px",
+      width: "50%",
     },
 
-    [`@media (min-width: ${mq.min[600]})`]: {
-      fontSize: "16px",
-      lineHeight: (28 / 16),
-      marginTop: "25px",
+    [`@media (max-width: ${mq.max["720"]})`]: {
+      fontSize: `${fontSizeBodySmall}px`,
+      lineHeight: lineHeightBodySmall,
+      marginTop: "12px",
     },
-  },
-
-  divider: {
-    borderColor: rgba(colors.accentGray, 0.27),
-    width: "50%",
-    display: "inline-block",
-    borderStyle: "solid",
-    borderTopWidth: 0,
-    marginBottom: "25px",
-    marginTop: "32px",
-
-    [`@media (min-width: ${mq.min[600]})`]: {
-      marginTop: "27px",
-    },
-  },
-
-  link: {
-    position: "relative",
-    display: "inline-block",
-  },
-
-  image: {
-    display: "block",
-    maxWidth: "100%",
-    maxHeight: "100%",
-    boxShadow: `0 12px 34px 0 ${rgba(colors.shadowPrimary, 0.12)},
-      inset 0 1px 3px 0 ${rgba(colors.shadowPrimary, 0.5)}`,
-    border: `1px solid ${rgba(colors.bgPrimary, 0.2)}`,
-  },
-
-  imageOverlay: {
-    backgroundColor: rgba(colors.bgOverlay, 0.16),
-    position: "absolute",
-    top: 0,
-    width: "100%",
-    height: "100%",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    transition: `background-color ${timing.default} ease`,
-  },
-
-  playIcon: {
-    color: colors.bgPrimary,
-    height: "21px",
-    width: "21px",
   },
 };
 
-const css = `
-  .SpotlightZone a:hover .SpotlightZone-imageOverlay {
-    background-color: ${rgba(colors.bgOverlay, 0.06)} !important;
-  }
-`;
-
-function markup(htmlContent) {
-  return {
-    __html: htmlContent,
-  };
-}
-
 const SpotlightZone = ({
   zone,
-  category,
   title,
   paragraph,
-  href,
-  imageUrl,
+  videoEmbed,
   backgroundImageUrl,
   adSlot,
   style,
@@ -264,93 +195,68 @@ const SpotlightZone = ({
     className="SpotlightZone"
     style={[styles.container, style]}
   >
-    <style dangerouslySetInnerHTML={markup(css)} />
-
     <Container style={styles.content}>
       <div style={styles.leftContent}>
-        <div style={styles.zone}>
-          {zone}
+        <div style={styles.leftContentTop}>
+          <div style={styles.zone}>
+            {zone}
+          </div>
+
+          <div style={styles.adSlot}>
+            {adSlot}
+          </div>
         </div>
 
-        <div style={styles.category}>
-          {category}
+        <div style={styles.leftContentMiddle}>
+          <Heading
+            level={2}
+            tracking="tight"
+            weight="thin"
+            override={styles.title}
+          >
+            {title}
+          </Heading>
+
+          <p
+            style={styles.paragraph}
+            dangerouslySetInnerHTML={{ __html: paragraph }}
+          />
         </div>
-
-        <Heading
-          level={2}
-          tracking="tight"
-          weight="thin"
-          override={styles.title}
-        >
-          {title}
-        </Heading>
-
-        <p
-          style={styles.paragraph}
-          dangerouslySetInnerHTML={{ __html: paragraph }}
-        />
-
-        {adSlot && <hr style={styles.divider} /> }
-        {adSlot}
       </div>
 
       <div style={styles.rightContent}>
-        <Link to={href} style={styles.link}>
-          <img
-            style={styles.image}
-            src={imageUrl}
-            alt=""
-          />
-
-          <div
-            className="SpotlightZone-imageOverlay"
-            style={styles.imageOverlay}
-          >
-            <Play style={styles.playIcon} />
-          </div>
-        </Link>
+        <VideoPopout
+          videoEmbed={videoEmbed}
+          style={{ height: "auto" }}
+        />
       </div>
     </Container>
 
     <div
       style={[
         styles.background,
-        { backgroundImage: `url("${backgroundImageUrl}")` },
+        { backgroundImage: backgroundImageUrl ? `url("${backgroundImageUrl}")` : "none" },
       ]}
       aria-hidden="true"
     />
-    <div style={styles.backgroundOverlay} />
   </div>
 );
 
 SpotlightZone.propTypes = {
   zone: PropTypes.string.isRequired,
-  category: PropTypes.string,
   title: PropTypes.string.isRequired,
   paragraph: PropTypes.string.isRequired,
-  href: PropTypes.string.isRequired,
-  imageUrl: PropTypes.string.isRequired,
+  videoEmbed: PropTypes.shape(VideoEmbed.propTypes).isRequired,
   backgroundImageUrl: PropTypes.string.isRequired,
   adSlot: PropTypes.element,
-  style: PropTypes.objectOf(
-    PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number,
-      PropTypes.object,
-    ]),
-  ),
+  style: propTypes.style,
 };
 
 SpotlightZone.defaultProps = {
   zone: "",
-  category: "",
   title: "",
   paragraph: "",
-  href: "",
-  imageUrl: "",
   backgroundImageUrl: "",
 };
-
-SpotlightZone.styles = styles;
 
 export default radium(SpotlightZone);
