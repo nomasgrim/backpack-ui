898e2b12869c51047365c6b220b1d5682d17a171
Tom Cunningham <thomasthesecond@users.noreply.github.com>
2018-01-10 09:39:43 -0600

    feat: Update CardVideo component (#591)

--- src/components/cardVideo/index.jsx
+++ src/components/cardVideo/index.jsx
@@ -1,73 +1,120 @@
 import React from "react";
 import PropTypes from "prop-types";
-import radium from "radium";
+import radium, { Style } from "radium";
 import cn from "classnames";
-
-import colors from "../../styles/colors";
-import mq from "../../styles/mq";
-import iconFromString from "../../utils/icon";
-import propTypes from "../../utils/propTypes";
-import duration from "../../utils/time";
 import {
   Card,
-  CardActionIcon,
   CardAnchor,
   CardBullets,
   CardHeading,
   CardImage,
   CardText,
 } from "../card";
-import TextBubble from "../textBubble";
-import { Play as PlayIcon } from "../icon";
-
-const mediaQuery = `@media (max-width: ${mq.max[768]})`;
+import {
+  fontSizeHeading7,
+  fontSizeBodySmall,
+  lineHeightBodySmall,
+} from "../../styles/typography";
+import IconButton from "../iconButton";
+import mq from "../../styles/mq";
+import colors from "../../styles/colors";
+import duration from "../../utils/time";
+import timing from "../../styles/timing";
+import { rgba } from "../../utils/color";
+import propTypes from "../../utils/propTypes";
 
 const styles = {
-  playIcon: {
-    bottom: "17px",
-    color: colors.bgPrimary,
-    fontSize: "20px",
-    left: "24px",
-    position: "absolute",
+  container: {
+    maxWidth: "100%",
+    minWidth: 0,
+  },
 
-    [mediaQuery]: {
-      bottom: "12px",
-      fontSize: "12px",
-      left: "11px",
+  image: {
+    position: "relative",
+  },
+
+  overlay: {
+    default: {
+      height: "100%",
+      left: 0,
+      position: "absolute",
+      top: 0,
+      width: "100%",
+    },
+
+    desktop: {
+      backgroundColor: rgba(colors.bgOverlay, 0.25),
+      opacity: 0,
+      transition: `opacity ${timing.fast} ease`,
+    },
+
+    mobile: {
+      backgroundImage: `linear-gradient(-180deg, ${rgba(colors.bgOverlay, 0)} 53%, ${rgba(colors.bgOverlay, 0.8)} 99%)`,
     },
   },
 
-  icon: {
-    verticalAlign: "bottom",
-  },
+  overlayContent: {
+    default: {
+      height: "100%",
+      left: 0,
+      position: "relative",
+      top: "10px",
+      transition: `top ${timing.fast} ease`,
+      width: "100%",
+    },
 
-  textBubble: {
-    bottom: "9px",
-    minWidth: "76px",
-    position: "absolute",
-    right: "10px",
-
-    [mediaQuery]: {
-      bottom: "6px",
-      fontSize: "9px",
-      minWidth: "53px",
-      paddingBottom: "5px",
-      paddingLeft: "15px",
-      paddingRight: "15px",
-      paddingTop: "7px",
-      right: "5px",
+    mobile: {
+      top: 0,
     },
   },
 
-  action: {
+  button: {
+    bottom: "16px",
+    fontSize: `${fontSizeHeading7}px`,
     position: "absolute",
-    right: "22px",
-    top: "25px",
 
-    [mediaQuery]: {
-      fontSize: "12px",
-      right: "9px",
-      top: "12px",
+    [`@media (max-width: ${mq.max["360"]})`]: {
+      bottom: "9px",
+      left: "10px",
+    },
+  },
+
+  playButton: {
+    left: "16px",
+    paddingLeft: "4px",
+  },
+
+  actionButton: {
+    right: "16px",
+  },
+
+  duration: {
+    default: {
+      color: colors.textOverlay,
+      fontSize: `${fontSizeBodySmall}px`,
+      lineHeight: lineHeightBodySmall,
+      position: "absolute",
+      right: "16px",
+
+      [`@media (max-width: ${mq.max["360"]})`]: {
+        right: "10px",
+      },
+    },
+
+    topAligned: {
+      top: "16px",
+
+      [`@media (max-width: ${mq.max["360"]})`]: {
+        top: "9px",
+      },
+    },
+
+    bottomAligned: {
+      bottom: "21px",
+
+      [`@media (max-width: ${mq.max["360"]})`]: {
+        bottom: "14px",
+      },
     },
   },
 };
@@ -77,65 +124,125 @@ const CardVideo = ({
   imageSrc,
   aspectRatio,
   actionIcon,
+  actionIconLabel,
   runtime,
   heading,
   bullets,
   onClick,
   layout,
+  theme,
+  spacing,
   className,
+  mobile,
   style,
 }) => (
   <Card
     className={cn("Card--video", className)}
     layout={layout}
-    style={style}
+    theme={theme}
+    style={[
+      styles.container,
+      style,
+    ]}
   >
     <CardImage
       href={href}
       src={imageSrc}
       aspectRatio={aspectRatio}
-      opacity={0.8}
+      opacity={1}
+      style={styles.image}
     >
       <div
-        className="PlayIcon"
-        style={styles.playIcon}
+        className="Card--video--overlay"
+        style={[
+          styles.overlay.default,
+          styles.overlay[mobile ? "mobile" : "desktop"],
+        ]}
       >
-        <PlayIcon style={styles.icon} />
-      </div>
+        <Style
+          scopeSelector=".Card--video:hover"
+          rules={{
+            ".Card--video--overlay": {
+              opacity: "1 !important",
+            },
+            ".Card--video--overlayContent": {
+              top: "0 !important",
+            },
+          }}
+        />
 
-      {typeof runtime === "number" &&
-        <TextBubble style={styles.textBubble}>
-          {duration(runtime)}
-        </TextBubble>
-      }
+        <div
+          className="Card--video--overlayContent"
+          style={[
+            styles.overlayContent.default,
+            mobile && styles.overlayContent.mobile,
+          ]}
+        >
+          <IconButton
+            hoverBackgroundScale={1.2}
+            shadow
+            iconName="Play"
+            label="Play"
+            style={[styles.button, styles.playButton]}
+            size={32}
+            hoverColor={colors.textOverlay}
+            hoverBackgroundColor={colors.linkPrimary}
+            transitionDuration={timing.fast}
+          />
+
+          {onClick &&
+            <IconButton
+              hoverBackgroundScale={1.2}
+              shadow
+              iconName={actionIcon}
+              label={actionIconLabel}
+              onClick={onClick}
+              style={[styles.button, styles.actionButton]}
+              size={32}
+              hoverColor={colors.textOverlay}
+              hoverBackgroundColor={colors.linkPrimary}
+              transitionDuration={timing.fast}
+            />
+          }
+
+          {typeof runtime === "number" &&
+            <div
+              style={[
+                styles.duration.default,
+                styles.duration[mobile || !onClick ? "bottomAligned" : "topAligned"],
+              ]}
+            >
+              {duration(runtime)}
+            </div>
+          }
+        </div>
+      </div>
     </CardImage>
 
     <CardText>
       <CardAnchor
         href={href}
         layout={layout}
-        style={styles.anchor}
+        spacing={spacing}
       >
         {bullets && bullets.length > 0 &&
-          <CardBullets bullets={bullets} />
+          <CardBullets
+            bullets={bullets}
+            spacing={spacing}
+          />
         }
 
-        <CardHeading>
+        <CardHeading
+          theme={theme}
+          spacing={spacing}
+          style={{
+            paddingLeft: "1px",
+            paddingRight: "1px",
+          }}
+        >
           {heading}
         </CardHeading>
       </CardAnchor>
-
-      {onClick &&
-        <CardActionIcon
-          style={[
-            styles.action,
-            layout === "tile" && { right: "3px" },
-          ]}
-          onClick={onClick}
-        >
-          {iconFromString(actionIcon)}
-        </CardActionIcon>
-      }
     </CardText>
   </Card>
 );
@@ -154,16 +261,30 @@ CardVideo.propTypes = {
     "card",
     "tile",
   ]),
+  theme: PropTypes.oneOf([
+    "light",
+    "dark",
+  ]),
+  spacing: PropTypes.oneOf([
+    "normal",
+    "compact",
+  ]),
   actionIcon: PropTypes.string,
+  actionIconLabel: PropTypes.string,
   onClick: PropTypes.func,
   className: PropTypes.string,
+  mobile: PropTypes.bool,
   style: propTypes.style,
 };
 
 CardVideo.defaultProps = {
   aspectRatio: "video",
   actionIcon: "ClockOutline",
+  actionIconLabel: "Watch Later",
   layout: "card",
+  theme: "light",
+  spacing: "normal",
+  mobile: false,
 };
 
 export default radium(CardVideo);
